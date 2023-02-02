1028865c0b7e54e8029b05695ca99bd43b72d744
totuta <totuta@gmail.com>
2023-02-01 22:47:21 -0800

    Fix indent errors in train.py

--- train.py
+++ train.py
@@ -10,122 +10,122 @@ import dill as pickle
 
 def train_model(model, opt):
 
-  print("training model...")
-  model.train()
-  start = time.time()
-  if opt.checkpoint > 0:
-    cptime = time.time()
-
-  for epoch in range(opt.epochs):
-
-    total_loss = 0
-    if opt.floyd is False:
-      print(" %dm: epoch %d [%s]  %d%%  loss = %s" % ((time.time() - start)//60, epoch+1, "".join(' '*20), 0, '...'), end='\r')
-
+    print("training model...")
+    model.train()
+    start = time.time()
     if opt.checkpoint > 0:
-      torch.save(model.state_dict(), 'weights/model_weights')
-
-    for i, batch in enumerate(opt.train):
-
-      src = batch.src.transpose(0,1)
-      trg = batch.trg.transpose(0,1)
-      trg_input = trg[:, :-1]
-      src_mask, trg_mask = create_masks(src, trg_input, opt)
-      preds = model(src, trg_input, src_mask, trg_mask)
-      ys = trg[:,1:].contiguous().view(-1)
-      opt.optimizer.zero_grad()
-      loss = F.cross_entropy(preds.view(-1, preds.size(-1)), ys, ignore_index=opt.trg_pad)
-      loss.backward()
-      opt.optimizer.step()
-      if opt.SGDR == True:
-        opt.sched.step()
-
-      total_loss += loss.item()
-      avg_loss = total_loss/opt.printevery
-      if (i + 1) % opt.printevery == 0:
-        p = int(100 * (i + 1) / opt.train_len)
-        # avg_loss = total_loss/opt.printevery
-        if opt.floyd is False:
-          print("   %dm: epoch %d [%s%s]  %d%%  loss = %.3f" %\
-          ((time.time() - start)//60, epoch + 1, "".join('#'*(p//5)), "".join(' '*(20-(p//5))), p, avg_loss), end='\r')
-        else:
-          print("   %dm: epoch %d [%s%s]  %d%%  loss = %.3f" %\
-          ((time.time() - start)//60, epoch + 1, "".join('#'*(p//5)), "".join(' '*(20-(p//5))), p, avg_loss))
-        total_loss = 0
-
-      if opt.checkpoint > 0 and ((time.time()-cptime)//60) // opt.checkpoint >= 1:
-        torch.save(model.state_dict(), 'weights/model_weights')
         cptime = time.time()
 
-    print("%dm: epoch %d [%s%s]  %d%%  loss = %.3f\nepoch %d complete, loss = %.03f" %\
-    ((time.time() - start)//60, epoch + 1, "".join('#'*(100//5)), "".join(' '*(20-(100//5))), 100, avg_loss, epoch + 1, avg_loss))
+    for epoch in range(opt.epochs):
+
+        total_loss = 0
+        if opt.floyd is False:
+            print(" %dm: epoch %d [%s]  %d%%  loss = %s" % ((time.time() - start)//60, epoch+1, "".join(' '*20), 0, '...'), end='\r')
+
+        if opt.checkpoint > 0:
+            torch.save(model.state_dict(), 'weights/model_weights')
+
+        for i, batch in enumerate(opt.train):
+
+            src = batch.src.transpose(0,1)
+            trg = batch.trg.transpose(0,1)
+            trg_input = trg[:, :-1]
+            src_mask, trg_mask = create_masks(src, trg_input, opt)
+            preds = model(src, trg_input, src_mask, trg_mask)
+            ys = trg[:,1:].contiguous().view(-1)
+            opt.optimizer.zero_grad()
+            loss = F.cross_entropy(preds.view(-1, preds.size(-1)), ys, ignore_index=opt.trg_pad)
+            loss.backward()
+            opt.optimizer.step()
+            if opt.SGDR == True:
+                opt.sched.step()
+
+            total_loss += loss.item()
+            avg_loss = total_loss/opt.printevery
+            if (i + 1) % opt.printevery == 0:
+                p = int(100 * (i + 1) / opt.train_len)
+                # avg_loss = total_loss/opt.printevery
+                if opt.floyd is False:
+                    print("   %dm: epoch %d [%s%s]  %d%%  loss = %.3f" %\
+                    ((time.time() - start)//60, epoch + 1, "".join('#'*(p//5)), "".join(' '*(20-(p//5))), p, avg_loss), end='\r')
+                else:
+                    print("   %dm: epoch %d [%s%s]  %d%%  loss = %.3f" %\
+                    ((time.time() - start)//60, epoch + 1, "".join('#'*(p//5)), "".join(' '*(20-(p//5))), p, avg_loss))
+                total_loss = 0
+
+            if opt.checkpoint > 0 and ((time.time()-cptime)//60) // opt.checkpoint >= 1:
+                torch.save(model.state_dict(), 'weights/model_weights')
+                cptime = time.time()
+
+        print("%dm: epoch %d [%s%s]  %d%%  loss = %.3f\nepoch %d complete, loss = %.03f" %\
+        ((time.time() - start)//60, epoch + 1, "".join('#'*(100//5)), "".join(' '*(20-(100//5))), 100, avg_loss, epoch + 1, avg_loss))
 
 def main():
 
-  parser = argparse.ArgumentParser()
-  parser.add_argument('-src_data', required=True)
-  parser.add_argument('-trg_data', required=True)
-  parser.add_argument('-src_lang', required=True)
-  parser.add_argument('-trg_lang', required=True)
-  parser.add_argument('-no_cuda', action='store_true')
-  parser.add_argument('-SGDR', action='store_true')
-  parser.add_argument('-epochs', type=int, default=2)
-  parser.add_argument('-d_model', type=int, default=512)
-  parser.add_argument('-n_layers', type=int, default=6)
-  parser.add_argument('-heads', type=int, default=8)
-  parser.add_argument('-dropout', type=int, default=0.1)
-  parser.add_argument('-batchsize', type=int, default=1500)
-  parser.add_argument('-printevery', type=int, default=100)
-  parser.add_argument('-lr', type=int, default=0.0001)
-  parser.add_argument('-load_weights')
-  parser.add_argument('-create_valset', action='store_true')
-  parser.add_argument('-max_strlen', type=int, default=80)
-  parser.add_argument('-floyd', action='store_true')
-  parser.add_argument('-checkpoint', type=int, default=0)
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-src_data', required=True)
+    parser.add_argument('-trg_data', required=True)
+    parser.add_argument('-src_lang', required=True)
+    parser.add_argument('-trg_lang', required=True)
+    parser.add_argument('-no_cuda', action='store_true')
+    parser.add_argument('-SGDR', action='store_true')
+    parser.add_argument('-epochs', type=int, default=2)
+    parser.add_argument('-d_model', type=int, default=512)
+    parser.add_argument('-n_layers', type=int, default=6)
+    parser.add_argument('-heads', type=int, default=8)
+    parser.add_argument('-dropout', type=int, default=0.1)
+    parser.add_argument('-batchsize', type=int, default=1500)
+    parser.add_argument('-printevery', type=int, default=100)
+    parser.add_argument('-lr', type=int, default=0.0001)
+    parser.add_argument('-load_weights')
+    parser.add_argument('-create_valset', action='store_true')
+    parser.add_argument('-max_strlen', type=int, default=80)
+    parser.add_argument('-floyd', action='store_true')
+    parser.add_argument('-checkpoint', type=int, default=0)
 
-  opt = parser.parse_args()
+    opt = parser.parse_args()
 
-  opt.device = torch.device('cuda') if opt.no_cuda is False else -1
-  if opt.device == torch.device('cuda'): # 0 means GPU is available
-    assert torch.cuda.is_available()
+    opt.device = torch.device('cuda') if opt.no_cuda is False else -1
+    if opt.device == torch.device('cuda'): # 0 means GPU is available
+        assert torch.cuda.is_available()
 
-  read_data(opt)
-  SRC, TRG = create_fields(opt)
-  opt.train = create_dataset(opt, SRC, TRG)
-  model = get_model(opt, len(SRC.vocab), len(TRG.vocab))
+    read_data(opt)
+    SRC, TRG = create_fields(opt)
+    opt.train = create_dataset(opt, SRC, TRG)
+    model = get_model(opt, len(SRC.vocab), len(TRG.vocab))
 
-  opt.optimizer = torch.optim.Adam(model.parameters(), lr=opt.lr, betas=(0.9, 0.98), eps=1e-9)
-  if opt.SGDR == True:
-    opt.sched = CosineWithRestarts(opt.optimizer, T_max=opt.train_len)
+    opt.optimizer = torch.optim.Adam(model.parameters(), lr=opt.lr, betas=(0.9, 0.98), eps=1e-9)
+    if opt.SGDR == True:
+        opt.sched = CosineWithRestarts(opt.optimizer, T_max=opt.train_len)
 
-  if opt.checkpoint > 0:
-    print("model weights will be saved every %d minutes and at end of epoch to directory weights/"%(opt.checkpoint))
+    if opt.checkpoint > 0:
+        print("model weights will be saved every %d minutes and at end of epoch to directory weights/"%(opt.checkpoint))
 
-  if opt.load_weights is not None and opt.floyd is not None:
-    os.mkdir('weights')
-    pickle.dump(SRC, open('weights/SRC.pkl', 'wb'))
-    pickle.dump(TRG, open('weights/TRG.pkl', 'wb'))
+    if opt.load_weights is not None and opt.floyd is not None:
+        os.mkdir('weights')
+        pickle.dump(SRC, open('weights/SRC.pkl', 'wb'))
+        pickle.dump(TRG, open('weights/TRG.pkl', 'wb'))
 
-  train_model(model, opt)
+    train_model(model, opt)
 
-  if opt.floyd is False:
-    promptNextAction(model, opt, SRC, TRG)
+    if opt.floyd is False:
+        promptNextAction(model, opt, SRC, TRG)
 
 def yesno(response):
-  while True:
-    if response != 'y' and response != 'n':
-      response = input('command not recognised, enter y or n :')
-    else:
-      return response
+    while True:
+        if response != 'y' and response != 'n':
+            response = input('command not recognised, enter y or n :')
+        else:
+            return response
 
 def promptNextAction(model, opt, SRC, TRG):
 
-  saved_once = 1 if opt.load_weights is not None or opt.checkpoint > 0 else 0
+    saved_once = 1 if opt.load_weights is not None or opt.checkpoint > 0 else 0
 
-  if opt.load_weights is not None:
-    dst = opt.load_weights
-  else:
-    dst = 'weights'
+    if opt.load_weights is not None:
+        dst = opt.load_weights
+    if opt.checkpoint > 0:
+        dst = 'weights'
 
     while True:
         save = yesno(input('training complete, save results? [y/n] : '))
@@ -178,4 +178,4 @@ def promptNextAction(model, opt, SRC, TRG):
 
     # for asking about further training use while true loop, and return
 if __name__ == "__main__":
-  main()
+    main()
