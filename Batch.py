428288930d052d3afe2538c4204ad8c6f62da869
totuta <totuta@gmail.com>
2023-02-02 21:30:35 -0800

    Fix tab from 2 to 4 for Batch.py

--- Batch.py
+++ Batch.py
@@ -5,55 +5,55 @@ import numpy as np
 from torch.autograd import Variable
 
 def nopeak_mask(size, opt):
-  np_mask = np.triu(np.ones((1, size, size)), k=1).astype('uint8')
-  np_mask = Variable(torch.from_numpy(np_mask) == 0)
-  if opt.device == torch.device('cuda'):
-    np_mask = np_mask.cuda()
-  return np_mask
+    np_mask = np.triu(np.ones((1, size, size)), k=1).astype('uint8')
+    np_mask = Variable(torch.from_numpy(np_mask) == 0)
+    if opt.device == torch.device('cuda'):
+        np_mask = np_mask.cuda()
+    return np_mask
 
 def create_masks(src, trg, opt):
-  src_mask = (src != opt.src_pad).unsqueeze(-2)
+    src_mask = (src != opt.src_pad).unsqueeze(-2)
 
-  if trg is not None:
-    trg_mask = (trg != opt.trg_pad).unsqueeze(-2)
-    size = trg.size(1)
-    np_mask = nopeak_mask(size, opt)
-    if trg.is_cuda:
-      np_mask.cuda()
-    if np_mask.is_cuda:
-      trg_mask = trg_mask.cuda()
-    trg_mask = trg_mask & np_mask
-  else:
-    trg_mask = None
-  return src_mask, trg_mask
+    if trg is not None:
+        trg_mask = (trg != opt.trg_pad).unsqueeze(-2)
+        size = trg.size(1)
+        np_mask = nopeak_mask(size, opt)
+        if trg.is_cuda:
+            np_mask.cuda()
+        if np_mask.is_cuda:
+            trg_mask = trg_mask.cuda()
+        trg_mask = trg_mask & np_mask
+    else:
+        trg_mask = None
+    return src_mask, trg_mask
 
 class MyIterator(data.Iterator):
-  def create_batches(self):
-    if self.train:
-      def pool(d, random_shuffler):
-        for p in data.batch(d, self.batch_size * 100):
-          p_batch = data.batch(
-            sorted(p, key=self.sort_key),
-            self.batch_size, self.batch_size_fn
-          )
-          for b in random_shuffler(list(p_batch)):
-            yield b
-      self.batches = pool(self.data(), self.random_shuffler)
+    def create_batches(self):
+        if self.train:
+            def pool(d, random_shuffler):
+                for p in data.batch(d, self.batch_size * 100):
+                    p_batch = data.batch(
+                        sorted(p, key=self.sort_key),
+                        self.batch_size, self.batch_size_fn
+                    )
+                    for b in random_shuffler(list(p_batch)):
+                        yield b
+            self.batches = pool(self.data(), self.random_shuffler)
 
-    else:
-      self.batches = []
-      for b in data.batch(self.data(), self.batch_size, self.batch_size_fn):
-        self.batches.append(sorted(b, key=self.sort_key))
+        else:
+            self.batches = []
+            for b in data.batch(self.data(), self.batch_size, self.batch_size_fn):
+                self.batches.append(sorted(b, key=self.sort_key))
 
 global max_src_in_batch, max_tgt_in_batch
 
 def batch_size_fn(new, count, sofar):
-  global max_src_in_batch, max_tgt_in_batch
-  if count == 1:
-    max_src_in_batch = 0
-    max_tgt_in_batch = 0
-  max_src_in_batch = max(max_src_in_batch, len(new.src))
-  max_tgt_in_batch = max(max_tgt_in_batch, len(new.trg) + 2)
-  src_elements = count * max_src_in_batch
-  tgt_elements = count * max_tgt_in_batch
-  return max(src_elements, tgt_elements)
+    global max_src_in_batch, max_tgt_in_batch
+    if count == 1:
+        max_src_in_batch = 0
+        max_tgt_in_batch = 0
+    max_src_in_batch = max(max_src_in_batch, len(new.src))
+    max_tgt_in_batch = max(max_tgt_in_batch, len(new.trg) + 2)
+    src_elements = count * max_src_in_batch
+    tgt_elements = count * max_tgt_in_batch
+    return max(src_elements, tgt_elements)
