94d73a24d32f170efae85cb719b289848649c237
totuta <totuta@gmail.com>
2023-02-02 21:31:04 -0800

    Fix tab from 2 to 4 for Tokenize.py

--- Tokenize.py
+++ Tokenize.py
@@ -2,18 +2,18 @@ import spacy
 import re
 
 class tokenize(object):
-  def __init__(self, lang):
-    if lang == 'en':
-      self.nlp = spacy.load("en_core_web_sm")
-    elif lang == 'fr':
-      self.nlp = spacy.load("fr_core_news_sm")
+    def __init__(self, lang):
+        if lang == 'en':
+            self.nlp = spacy.load("en_core_web_sm")
+        elif lang == 'fr':
+            self.nlp = spacy.load("fr_core_news_sm")
 
-  def tokenizer(self, sentence):
-    sentence = re.sub(
-    r"[\*\"“”\n\\…\+\-\/\=\(\)‘•:\[\]\|’\!;]", " ", str(sentence))
-    sentence = re.sub(r"[ ]+", " ", sentence)
-    sentence = re.sub(r"\!+", "!", sentence)
-    sentence = re.sub(r"\,+", ",", sentence)
-    sentence = re.sub(r"\?+", "?", sentence)
-    sentence = sentence.lower()
-    return [tok.text for tok in self.nlp.tokenizer(sentence) if tok.text != " "]
+    def tokenizer(self, sentence):
+        sentence = re.sub(
+        r"[\*\"“”\n\\…\+\-\/\=\(\)‘•:\[\]\|’\!;]", " ", str(sentence))
+        sentence = re.sub(r"[ ]+", " ", sentence)
+        sentence = re.sub(r"\!+", "!", sentence)
+        sentence = re.sub(r"\,+", ",", sentence)
+        sentence = re.sub(r"\?+", "?", sentence)
+        sentence = sentence.lower()
+        return [tok.text for tok in self.nlp.tokenizer(sentence) if tok.text != " "]
